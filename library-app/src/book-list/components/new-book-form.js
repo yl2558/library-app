8d96820f2b4e7ad83d67c2493d27389e475a5ce9
Lydia <emailvar>
2019-02-15 22:38:21 -0500

    create a reusable component for create/update a book

---
+++ library-app/src/book-list/components/new-book-form.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import _ from "lodash";
+import Form from "antd/lib/form";
+import Button from "antd/lib/button";
+import Input from "antd/lib/input";
+
+class NewBookForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleOnSubmit = this.handleOnSubmit.bind(this);
+  }
+
+  handleOnSubmit(e) {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (_.isEmpty(err)) {
+        this.props.onSubmit(values);
+        this.props.form.resetFields();
+      }
+    });
+  }
+
+  render() {
+    const FormItem = Form.Item;
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 6 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 }
+      }
+    };
+    const { bookInfo } = this.props;
+    return (
+      <Form onSubmit={this.handleOnSubmit}>
+        <FormItem {...formItemLayout} label="Title">
+          {getFieldDecorator("title", {
+            rules: [
+              {
+                required: true,
+                message: "Please input Book Title!"
+              }
+            ],
+            initialValue: bookInfo.title
+          })(<Input placeholder="Enter Book Title" />)}
+        </FormItem>
+        <FormItem {...formItemLayout} label="Author">
+          {getFieldDecorator("author", {
+            rules: [
+              {
+                required: true,
+                message: "Please input Book Author!"
+              }
+            ],
+            initialValue: bookInfo.author
+          })(<Input placeholder="Enter Book Author" />)}
+        </FormItem>
+        <FormItem {...formItemLayout} label="Description">
+          {getFieldDecorator("description", {
+            rules: [
+              {
+                required: true,
+                message: "Please input Book Description!"
+              }
+            ],
+            initialValue: bookInfo.description
+          })(<Input.TextArea placeholder="Enter Book Description" />)}
+        </FormItem>
+        <FormItem>
+          <Button type="primary" htmlType="submit" style={{ float: "right" }}>
+            Add
+          </Button>
+        </FormItem>
+      </Form>
+    );
+  }
+}
+
+NewBookForm = Form.create()(NewBookForm);
+export { NewBookForm };
+export default NewBookForm;
