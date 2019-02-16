11f1a44fba52017448c15bc64b6f1b71b4dbf6db
Lydia <emailvar>
2019-02-16 15:47:08 -0500

    show single book details

--- library-app/src/book-list/components/book.js
+++ library-app/src/book-list/components/book.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import _ from "lodash";
 import { connect } from "react-redux";
+import Card from "antd/lib/card";
+import Button from "antd/lib/button";
+import "./book-list.scss";
 
 class Book extends Component {
   constructor(props) {
@@ -9,12 +12,43 @@ class Book extends Component {
     this.state = {};
   }
 
+  handleOnClick = () => {
+    this.props.history.push("/");
+  };
+
   render() {
     const { book } = this.props;
     if (_.isEmpty(book)) {
       return <div>Loading...</div>;
     }
-    return <div>{book.title}</div>;
+    return (
+      <div>
+        <div style={{ display: "flex" }} className="center mb-10">
+          <div style={{ width: "50%" }}>
+            <Button onClick={this.handleOnClick}>Back</Button>
+          </div>
+        </div>
+        <div style={{ display: "flex" }} className="center">
+          <Card style={{ width: "50%" }}>
+            <div style={{ display: "flex" }}>
+              <div style={{ flex: "30% 1 1" }}>
+                <img
+                  src={book.imageLink}
+                  alt={book.id}
+                  height="100%"
+                  width="100%"
+                />
+              </div>
+              <div className="mb-10 ml-10" style={{ flex: "70% 1 1" }}>
+                <h3>{book.title}</h3>
+                <div className="mb-10">by {book.author}</div>
+                <div>{book.description}</div>
+              </div>
+            </div>
+          </Card>
+        </div>
+      </div>
+    );
   }
 }
 
